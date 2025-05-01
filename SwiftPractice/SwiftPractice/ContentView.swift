b5eb437002f3827ff7c821021307ecd31963dcd4
松浦 <2210344@ecc.ac.jp>
2025-05-01 14:55:10 +0900

    add .gitignore追加

---
+++ SwiftPractice/SwiftPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPractice
+//
+//  Created by 松浦壮吾 on 2025/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPractice/SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by 松浦壮吾 on 2025/05/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
