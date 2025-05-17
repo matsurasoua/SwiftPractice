adccd338bed3319596d4d6434a35b98e84bf369c
松浦 <2210344@ecc.ac.jp>
2025-05-17 15:09:05 +0900

    add

---
+++ SwiftPractice/SwiftPractice/practice5.playground/Contents.swift
@@ -0,0 +1,18 @@
+// MARK: switch
+
+enum Shape {
+    case circle
+    case square
+    case triangle
+}
+
+var switchvar: Shape = .circle
+
+switch switchvar{
+    case .circle:
+    print("Circle")
+case .square:
+    print("Square")
+case .triangle:
+    print("Triangle")
+}
